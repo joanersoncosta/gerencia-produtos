ec3c5c60ba075dffd9d9e98edc13b10c48121877
Joanerson Costa <joanersonc31@gmail.com>
2024-10-14 09:39:50 -0300

    feat: cria vpc e cluster

---
+++ src/main/java/com/myorg/VpcStack.java
@@ -0,0 +1,34 @@
+package com.myorg;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.constructs.Construct;
+// import software.amazon.awscdk.Duration;
+// import software.amazon.awscdk.services.sqs.Queue;
+
+public class VpcStack extends Stack {
+  private Vpc vpc;
+
+    public VpcStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public VpcStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+        vpc = Vpc.Builder.create(this, "vpc01")
+                .maxAzs(3)
+                .build();
+        // The code that defines your stack goes here
+
+        // example resource
+        // final Queue queue = Queue.Builder.create(this, "CursoAwsCdkQueue")
+        //         .visibilityTimeout(Duration.seconds(300))
+        //         .build();
+    }
+
+    public Vpc getVpc() {
+        return vpc;
+    }
+}
